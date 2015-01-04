033fdd053e91f4a5774bb40c3eb569886a15f9cd
anlhord <anlhord@gmail.com>
2015-01-04 16:07:07 +0100

    Add the install script

---
+++ install.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+wget -c https://storage.googleapis.com/appengine-sdks/featured/go_appengine_sdk_linux_amd64-1.9.17.zip 
+unzip go_appengine_sdk_linux_amd64-1.9.17.zip
+
+wget -c http://storage.googleapis.com/nativeclient-mirror/nacl/nacl_sdk/nacl_sdk.zip
+unzip nacl_sdk.zip
+
